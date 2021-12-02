50bbd429e1672b6735841c912e5d5b6fce5e5271
zsping1989 <214986304@qq.com>
2021-12-03 00:23:17 +0800

    init

---
+++ resources/js/pages/open/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="open_index">
+        {{$tp('Home page of official website')}}
+        <router-link to="/admin">{{$tp('Backstage home page')}}</router-link>
+        <router-link to="/home">{{$tp('Front desk home page')}}</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+        },
+        data(){
+            return {
+                "{lang_path}":'open.index',
+            };
+        }
+    };
+</script>
